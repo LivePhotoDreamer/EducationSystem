a452b242528a67899bbd7bb13b4d522f5b0cc174
Bertram Gilfoyle <programmingdutop@gmail.com>
2021-05-18 00:00:27 +0300

    #1: Added missed Student class.

---
+++ EducationalSystem/EducationalSystemLib/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationalSystemTables.Model
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public int User_Information_Id { get; set; }
+        public int Group_Id { get; set; }
+    }
+}
