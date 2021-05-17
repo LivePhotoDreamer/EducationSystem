714f7ce5a96c9832d774898e72a4d9a7c98dfc44
Bertram Gilfoyle <programmingdutop@gmail.com>
2021-05-17 22:44:09 +0300

    #0: Created init project car car car car Dima Top

---
+++ EducationalSystem/EducationalSystemLib/Model/User_Information.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationalSystemTablesTables.Model
+{
+    class User_Information
+    {
+        public int Id { get; set; }
+        public int User_Type_Id { get; set; }
+
+    }
+}
